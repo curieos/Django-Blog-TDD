5191ea119937be515ae2fb74d94b7b6e43eedb35
Caroline <caroline@curieos.com>
2019-06-23 16:23:54 -0500

    Auto set date_created and add related projects

--- models.py
+++ models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from datetime import datetime
+
+def get_current_date_time():
+		return datetime.now()
 
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=50)
-	projects = models.ManyToManyField("projects.Project", verbose_name="Related Projects")
+	projects = models.ManyToManyField("projects.Project", verbose_name="Related Projects", blank=True)
 	tags = models.ManyToManyField("Tag")
-	date_created = models.DateTimeField(primary_key=True)
+	date_created = models.DateTimeField(primary_key=True, default=get_current_date_time)
 	last_modified = models.DateTimeField(auto_now=True)
 	content = models.TextField()
 
