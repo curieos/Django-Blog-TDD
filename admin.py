f3f8d6898e8318b1227972e8ebd69e3d86f19182
Caroline <caroline@curieos.com>
2019-07-08 12:08:47 -0500

    Add markdown support to the blog

--- admin.py
+++ admin.py
@@ -1,7 +1,14 @@
+from django.db import models
 from django.contrib import admin
+from markdownx.widgets import AdminMarkdownxWidget
 
 from .models import Tag, Post
 
+class MyModelAdmin(admin.ModelAdmin):
+	formfield_overrides = {
+		models.TextField: {'widget': AdminMarkdownxWidget},
+	}
+
 # Register your models here.
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, MyModelAdmin)
