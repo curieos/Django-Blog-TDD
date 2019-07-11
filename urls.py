cddaebaccbad09593387c1b6da240024ac586791
Caroline <caroline@curieos.com>
2019-07-11 13:13:44 -0500

    Forgot to save after merge

--- urls.py
+++ urls.py
@@ -4,11 +4,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-<<<<<<< HEAD
 	path("", views.IndexView.as_view(), name="index"),
 	path("<int:year>/<int:month>/<slug:slug>", views.PostDetail.as_view(), name="post"),
 	path("<slug:slug>", views.TagDetail.as_view(), name="tag")
-=======
-    path("", views.IndexView.as_view(), name="index"),
->>>>>>> 9004bf328deb9a9a4f46a19a888042c1dfcd492d
 ]
--- views.py
+++ views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
 from django.views import generic
-<<<<<<< HEAD
 
 from .models import Post, Tag
 
@@ -24,14 +23,3 @@ class TagDetail(generic.DetailView):
 	template_name = "blog/tag.html"
 	context_object_name = "tag"
 
-=======
-
-from .models import Post
-
-class IndexView(generic.ListView):
-    template_name = "blog/index.html"
-    context_object_name = "post_list"
-
-    def get_queryset(self):
-        return Post.objects.order_by("-date_created")[:6]
->>>>>>> 9004bf328deb9a9a4f46a19a888042c1dfcd492d
